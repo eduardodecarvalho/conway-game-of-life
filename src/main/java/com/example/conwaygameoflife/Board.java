adf67d0a5335762cd635b1e25aa119a63b03c6ff
eduardo <decarvalho.dev@gmail.com>
2022-08-31 14:13:39 +0100

    print board

--- src/main/java/com/example/conwaygameoflife/Board.java
+++ src/main/java/com/example/conwaygameoflife/Board.java
@@ -11,4 +11,25 @@ public class Board {
         this.height = height;
         this.board = new int[width][height];
     }
+
+    public void print() {
+        for (int x = 0; x < height; x++) {
+            StringBuilder line = new StringBuilder("|");
+            for (int y = 0; y < width; y++) {
+                line.append(this.board[x][y] == 0 ? "-" : "X");
+            }
+            line.append("|");
+            System.out.println(line);
+        }
+    }
+
+    public static void main(String[] args) {
+        Board board = new Board(10,10);
+
+        System.out.println("-----\n");
+
+        board.print();
+
+        System.out.println("\n-----");
+    }
 }
